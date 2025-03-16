ee6429739003118569a4662c5cd2d188c7bcc58a
ChrisAdderley <cadderley@gmail.com>
2025-03-16 09:15:08 -0700

    Remove srb smoke template, add Engine On Off Controller

--- Source/Waterfall/EffectControllers/EngineOnOffController.cs
+++ Source/Waterfall/EffectControllers/EngineOnOffController.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Linq;
-using UnityEngine;
 
 namespace Waterfall
 {
   [DisplayName("Engine On State")]
   public class EngineOnOffController : WaterfallController
   {
-    public float currentState = 0f;
     [Persistent] public string engineID = String.Empty;
-    [Persistent] public float responseRateUp = 100f;
-    [Persistent] public float responseRateDown = 100f;
     private ModuleEngines engineController;
 
     public bool zeroOnFlameout = true;
@@ -31,10 +27,6 @@ namespace Waterfall
         Utils.Log($"[EngineOnOffController] Could not find engine ID {engineID}, using first module", LogType.Effects);
         engineController = host.part.FindModuleImplementing<ModuleEngines>();
       }
-      else
-      {
-        currentState = engineController.isOperational ? 1f : 0f;
-      }
 
       if (engineController == null)
         Utils.LogError("[EngineOnOffController] Could not find engine controller on Initialize");
@@ -49,17 +41,13 @@ namespace Waterfall
       }
       else if (zeroOnFlameout)
       {
-        float targetThrottle = engineController.isOperational && !engineController.flameout ? 1f : 0f;
-
-        if (currentState != targetThrottle)
-        {
-          float rampRate = targetThrottle > currentState ? responseRateUp : responseRateDown;
-          currentState = Mathf.MoveTowards(currentState, targetThrottle, rampRate * TimeWarp.deltaTime);
-        }
-
-        return currentState;
+        return engineController.isOperational && !engineController.flameout ? 1f : 0f;
       }
-      return currentState;
+      else
+      {
+        return engineController.isOperational  ? 1f : 0f;
+      }
+      
     }
   }
 }
