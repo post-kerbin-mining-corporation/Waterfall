c4c33b927bc1b0f11eff9e60f80cf2a27ced0752
ChrisAdderley <cadderley@gmail.com>
2025-03-16 09:15:08 -0700

    Remove srb smoke template, add Engine On Off Controller

---
+++ Source/Waterfall/EffectControllers/EngineOnOffController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Waterfall
+{
+  [DisplayName("Engine On State")]
+  public class EngineOnOffController : WaterfallController
+  {
+    [Persistent] public string engineID = String.Empty;
+    private ModuleEngines engineController;
+
+    public bool zeroOnFlameout = true;
+
+    public EngineOnOffController() : base() { }
+    public EngineOnOffController(ConfigNode node) : base(node) { }
+
+    public override void Initialize(ModuleWaterfallFX host)
+    {
+      base.Initialize(host);
+
+      values = new float[1];
+
+      engineController = host.GetComponents<ModuleEngines>().FirstOrDefault(x => x.engineID == engineID);
+      if (engineController == null)
+      {
+        Utils.Log($"[EngineOnOffController] Could not find engine ID {engineID}, using first module", LogType.Effects);
+        engineController = host.part.FindModuleImplementing<ModuleEngines>();
+      }
+
+      if (engineController == null)
+        Utils.LogError("[EngineOnOffController] Could not find engine controller on Initialize");
+    }
+
+    protected override float UpdateSingleValue()
+    {
+      if (engineController == null)
+      {
+        Utils.LogWarning("[EngineOnOffController] Engine controller not assigned");
+        return 0f;
+      }
+      else if (zeroOnFlameout)
+      {
+        return engineController.isOperational && !engineController.flameout ? 1f : 0f;
+      }
+      else
+      {
+        return engineController.isOperational  ? 1f : 0f;
+      }
+      
+    }
+  }
+}
+   
---
+++ Source/Waterfall/UI/EffectControllersUI/EngineOnOffControllerUIOptions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UniLinq;
+
+namespace Waterfall.UI.EffectControllersUI
+{
+  public class EngineOnOffControllerUIOptions : DefaultEffectControllerUIOptions<EngineOnOffController>
+  {
+    private string[] engineIDOptions;
+    private int engineIndex;
+    private bool offOnFlameout;
+
+    public EngineOnOffControllerUIOptions() { }
+
+    public override void DrawOptions()
+    {
+      GUILayout.BeginHorizontal();
+      GUILayout.Label("Engine ID", UIResources.GetStyle("data_header"), GUILayout.MaxWidth(160f));
+      if (engineIDOptions != null && engineIDOptions.Length != 0)
+      {
+        engineIndex = GUILayout.SelectionGrid(engineIndex, engineIDOptions, 2);
+      }
+      else
+      {
+        GUILayout.Label("0");
+      }
+      GUILayout.EndHorizontal();
+      offOnFlameout = GUILayout.Toggle(offOnFlameout, "Zero on Flameout");
+    }
+
+    protected override void LoadOptions(EngineOnOffController controller)
+    {
+      List<ModuleEngines> engineOptions = controller.ParentModule.part.FindModulesImplementing<ModuleEngines>();
+      engineIDOptions = engineOptions.Select(x => x.engineID).ToArray();
+      engineIndex = engineIDOptions.ToList().IndexOf(controller.engineID);
+      engineIndex = engineIndex == -1 ? 0 : engineIndex;
+      offOnFlameout = controller.zeroOnFlameout;
+    }
+
+    public override void DefaultOptions(ModuleWaterfallFX parentModule)
+    {
+      List<ModuleEngines> engineOptions = parentModule.part.FindModulesImplementing<ModuleEngines>();
+      engineIDOptions = engineOptions.Select(x => x.engineID).ToArray();
+      engineIndex = 0;
+      offOnFlameout = true;
+    }
+
+    protected override EngineOnOffController CreateControllerInternal() =>
+      new()
+      {
+        engineID = engineIDOptions[engineIndex]
+      };
+  }
+}
