6c76c6ba5d226701664734d33374b9411a98bb15
ChrisAdderley <cadderley@gmail.com>
2025-03-16 16:38:07 -0700

    Add ramp rates to On/Off controller

--- Source/Waterfall/UI/EffectControllersUI/EngineOnOffControllerUIOptions.cs
+++ Source/Waterfall/UI/EffectControllersUI/EngineOnOffControllerUIOptions.cs
@@ -7,11 +7,18 @@ namespace Waterfall.UI.EffectControllersUI
 {
   public class EngineOnOffControllerUIOptions : DefaultEffectControllerUIOptions<EngineOnOffController>
   {
+    private readonly string[] rampStrings;
     private string[] engineIDOptions;
     private int engineIndex;
     private bool offOnFlameout;
+    private float rampRateUp = 100f;
+    private float rampRateDown = 100f;
 
-    public EngineOnOffControllerUIOptions() { }
+
+    public EngineOnOffControllerUIOptions() 
+    {
+      rampStrings = new[] { rampRateUp.ToString(), rampRateDown.ToString() };
+    }
 
     public override void DrawOptions()
     {
@@ -27,10 +34,33 @@ namespace Waterfall.UI.EffectControllersUI
       }
       GUILayout.EndHorizontal();
       offOnFlameout = GUILayout.Toggle(offOnFlameout, "Zero on Flameout");
+
+      GUILayout.BeginHorizontal();
+      GUILayout.Label("Ramp Rate Up", UIResources.GetStyle("data_header"), GUILayout.MaxWidth(160f));
+      rampStrings[0] = GUILayout.TextArea(rampStrings[0], GUILayout.MaxWidth(60f));
+
+      if (Single.TryParse(rampStrings[0], out float floatParsed))
+      {
+        rampRateUp = floatParsed;
+      }
+
+      GUILayout.EndHorizontal();
+
+      GUILayout.BeginHorizontal();
+      GUILayout.Label("Ramp Rate Down", UIResources.GetStyle("data_header"), GUILayout.MaxWidth(160f));
+      rampStrings[1] = GUILayout.TextArea(rampStrings[1], GUILayout.MaxWidth(60f));
+      if (Single.TryParse(rampStrings[1], out floatParsed))
+      {
+        rampRateDown = floatParsed;
+      }
+
+      GUILayout.EndHorizontal();
     }
 
     protected override void LoadOptions(EngineOnOffController controller)
     {
+      rampStrings[0] = controller.responseRateUp.ToString();
+      rampStrings[1] = controller.responseRateDown.ToString();
       List<ModuleEngines> engineOptions = controller.ParentModule.part.FindModulesImplementing<ModuleEngines>();
       engineIDOptions = engineOptions.Select(x => x.engineID).ToArray();
       engineIndex = engineIDOptions.ToList().IndexOf(controller.engineID);
@@ -49,7 +79,9 @@ namespace Waterfall.UI.EffectControllersUI
     protected override EngineOnOffController CreateControllerInternal() =>
       new()
       {
-        engineID = engineIDOptions[engineIndex]
+        engineID = engineIDOptions[engineIndex],
+        responseRateUp = rampRateUp,
+        responseRateDown = rampRateDown
       };
   }
 }
