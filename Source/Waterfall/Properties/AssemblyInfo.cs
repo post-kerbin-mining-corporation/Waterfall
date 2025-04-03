5ccc75eb46127010e71c4285ce1ddc1676a089d1
JonnyOThan <jonnyothan@gmail.com>
2025-04-03 15:20:12 -0400

    hook up versioning generation

--- Source/Waterfall/Properties/AssemblyInfo.cs
+++ Source/Waterfall/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright ©2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,15 +18,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ca27a9c5-b2a2-45d8-a9ac-2053bb93c2d6")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.10.2")]
-[assembly: AssemblyFileVersion("0.10.2")]
