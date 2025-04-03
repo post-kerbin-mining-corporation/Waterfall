a6b1f06c26c3898c60b2e3f1ec623df088b14c9b
JonnyOThan <jonnyothan@gmail.com>
2025-04-03 14:18:59 -0400

    WIP switch to kspbt

--- Source/Waterfall/Properties/AssemblyInfo.cs
+++ Source/Waterfall/Properties/AssemblyInfo.cs
@@ -5,11 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Waterfall")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Waterfall")]
 [assembly: AssemblyCopyright("Copyright ©2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
